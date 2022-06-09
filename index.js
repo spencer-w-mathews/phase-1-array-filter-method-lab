a07ef588729f44f5c00aca2dc1bf7f1a26ee18c8
Spencer Mathews <smathews0906@gmail.com>
2022-06-08 20:34:10 -0500

    commit tree 982ea5df18b90c25c26d9873df78f6187d9da0b8

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+
+/*function findMatching(names, string){
+
+    names.filter(function(name){
+        return (name.toUppercase === string.toUppercase);
+     
+    });
+}*/
+
+
+function findMatching(array, string) {
+    const matching = array.filter((driver)=> (driver.toLowerCase() === string.toLowerCase()))
+    return(matching)
+}
+
+function fuzzyMatch(array, string){
+    const startsWith = array.filter((driverName) => driverName.startsWith(string));
+
+    return(startsWith);
+}
+
+function matchName(array, string){
+    const nameMatch= array.filter((driverName) => (driverName.name===string));
+    return(nameMatch)
+}
